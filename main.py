5538cab2e06060c340c42426e3224994e979677c
Luiz Paulo Prado <lp1717@gmail.com>
2025-10-19 17:48:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, HTTPException, Query
-from pydantic import BaseModel, Field
+from fastapi import FastAPI, HTTPException, Query, status
+from pydantic import BaseModel, Field, field_validator
 from typing import Annotated
 
 
@@ -8,9 +8,12 @@ class UserIn(BaseModel):
     name: str = Field(min_length=2)
     age: int = Field(gt=0)
 
-    def custom_validation(self):
-        if self.age > 99:
-            raise HTTPException(status_code=500, detail="Age must be less than 99")
+    @field_validator("age")
+    @classmethod
+    def validate_age(cls, v: int):
+        if v > 100:
+            raise ValueError("Age cannot be more than 100")
+        return v
 
 
 # wire_out
@@ -34,15 +37,13 @@ app = FastAPI()
 
 
 @app.get("/users", response_model=list[UserOut])
-def get_users(skip: int = 0, limit: int = 10) -> list[UserOut]:
+def get_users(skip: int = 0, limit: int = 10):
     result = list(db.values())
     return result[skip : skip + limit]
 
 
-@app.post("/users", response_model=UserOut)
-def post_user(user: UserIn) -> UserOut:
-    user.custom_validation()
-
+@app.post("/users", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+def post_user(user: UserIn):
     next_id = max(db.keys()) + 1
     new_user = UserOut(
         id=next_id,
@@ -55,7 +56,7 @@ def post_user(user: UserIn) -> UserOut:
 
 
 @app.get("/users/{user_id}", response_model=UserOut)
-def get_user(user_id: int) -> UserOut:
+def get_user(user_id: int):
     if user_id not in db.keys():
         raise HTTPException(status_code=404, detail="User not found!")
 
@@ -63,7 +64,7 @@ def get_user(user_id: int) -> UserOut:
 
 
 @app.put("/users/{user_id}", response_model=UserOut)
-def put_user(user_id: int, user: UserIn) -> UserOut:
+def put_user(user_id: int, user: UserIn):
     if user_id not in db.keys():
         raise HTTPException(status_code=404, detail="User not found!")
 
@@ -78,7 +79,7 @@ def put_user(user_id: int, user: UserIn) -> UserOut:
     return db[user_id]
 
 
-@app.delete("/users/{user_id}")
+@app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(user_id: int):
     if user_id not in db.keys():
         raise HTTPException(status_code=404, detail="User not found!")
@@ -87,7 +88,7 @@ def delete_user(user_id: int):
 
 
 @app.get("/users/search/", response_model=list[UserOut])
-def search_user(name: Annotated[str, Query(min_length=2)]) -> list[UserOut]:
+def search_user(name: Annotated[str, Query(min_length=2)]):
     result = [
         user for user in db.values() if user.name.lower().startswith(name.lower())
     ]
