966fdeb7ba4053602fac22f803d61c8be5dd67a4
Luiz Paulo Prado <lp1717@gmail.com>
2025-10-19 00:22:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
-from typing import Annotated
+from fastapi import FastAPI, Query, HTTPException
+from dataclasses import dataclass
+
 
 app = FastAPI()
 
 
 # wire_in
-class UserIn(BaseModel):
+@dataclass
+class UserIn:
     name: str
     age: int
 
+    def validate(self):
+        if not self.name or not self.age or self.age == 0:
+            raise HTTPException(status_code=500, detail="All fields are mandatory")
+
 
 # wire_out
-class UserOut(BaseModel):
+@dataclass
+class UserOut:
     id: int
     name: str
     age: int
@@ -36,12 +42,15 @@ def get_users(skip: int = 0, limit: int = 10):
 
 @app.post("/users")
 def post_user(user: UserIn):
+    user.validate()
+
     next_id = max(db.keys()) + 1
     new_user = UserOut(
         id=next_id,
         name=user.name,
         age=user.age,
     )
+
     db[new_user.id] = new_user
     next_id += 1
 
@@ -51,7 +60,7 @@ def post_user(user: UserIn):
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     if user_id not in db.keys():
-        return "Error"
+        raise HTTPException(status_code=500, detail="User does not exists!")
 
     return db[user_id]
 
@@ -59,13 +68,16 @@ def get_user(user_id: int):
 @app.put("/users/{user_id}")
 def put_user(user_id: int, user: UserIn):
     if user_id not in db.keys():
-        return "Error"
+        raise HTTPException(status_code=500, detail="User does not exists!")
+
+    user.validate()
 
     updated_user = UserOut(
         id=user_id,
         name=user.name,
         age=user.age,
     )
+
     db[user_id] = updated_user
 
     return db[user_id]
@@ -74,13 +86,18 @@ def put_user(user_id: int, user: UserIn):
 @app.delete("/users/{user_id}")
 def delete_user(user_id: int):
     if user_id not in db.keys():
-        return "Error"
+        raise HTTPException(status_code=500, detail="User does not exists!")
 
     del db[user_id]
 
 
 @app.get("/users/search/")
-def search_user(name: Annotated[str, Query(min_length=2)]):
+def search_user(name: str):
+    if not name or len(name) < 2:
+        raise HTTPException(
+            status_code=500, detail="Name is mandatory and needs to have more than 2 characters"
+        )
+
     result = [
         user for user in db.values() if user.name.lower().startswith(name.lower())
     ]
